104bae264eef62e40e9a2ccf8988672757315349
ivorycoffee <jskim331933@naver.com>
2026-04-05 01:46:47 +0900

    EditorLocalization.cs 에서 LocalizationDataPath에 /Packages/ 추가, Reward.cs에서 StartAd() 안에 LoadRewardAd() 포함

--- Assets/Scripts/Advertisement/Reward.cs
+++ Assets/Scripts/Advertisement/Reward.cs
@@ -20,7 +20,7 @@ public class Reward : MonoBehaviour
         private RewardedAd _rewardedAd;
 
         private Action _onUserEarnedReward;
-
+    
     public void LoadRewardAd()
     {
         LoadAd();
@@ -41,7 +41,7 @@ public class Reward : MonoBehaviour
         {
             // This callback is called once the MobileAds SDK is initialized.
         });
-
+        LoadRewardAd();
         // Create our request used to load the ad.
 
     }
--- Assets/Scripts/MainUI/GameOverUI.cs
+++ Assets/Scripts/MainUI/GameOverUI.cs
@@ -45,6 +45,16 @@ public class GameOverUI : MonoBehaviour
         Time.timeScale = 0f;
     }
 
+    // "광고 보고 계속하기" 공통 버튼
+    public void OnClickWatchAd()
+    {   
+        // 광고를 보고 나서 콜백으로 부활 처리
+            if(!RemoveAd.isAdRemoved){
+                reward.ShowRewardAd(OnRewardSuccess);
+            }
+            else OnRewardSuccess();
+    }
+    
     // "그냥 나가기" 버튼에 연결할 함수
     public void OnClickQuit()
     {
